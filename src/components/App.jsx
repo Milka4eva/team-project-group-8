1d2e6d3c8e371054f71ae104457d48c80ba28e84
Olya <katniss30051991@gmail.com>
2023-08-27 21:54:55 +0300

    Clean app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,4 @@
-import { useDispatch } from 'react-redux';
-import { register, login, logout } from 'redux/auth/auth-operations';
-
 export const App = () => {
-  const dispatch = useDispatch();
   return (
     <div
       style={{
@@ -14,36 +10,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(
-            register({
-              name: 'Adrian Cross',
-              email: 'across@mail.com',
-              password: 'examplepwD12345',
-            })
-          )
-        }
-      >
-        Register
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(
-            login({
-              email: 'across@mail.com',
-              password: 'examplepwD12345',
-            })
-          )
-        }
-      >
-        Login
-      </button>
-      <button type="button" onClick={() => dispatch(logout())}>
-        Logout
-      </button>
+      React homework template
     </div>
   );
 };
