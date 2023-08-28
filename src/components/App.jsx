170ec10a0d5ea5e1f297cc2d60590fab4ce2d77d
Olya <katniss30051991@gmail.com>
2023-08-28 21:24:10 +0300

    Merge with main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import CalendarPage from './../Pages/CalendarPage/CalendarPage';
 import { Route, Routes } from 'react-router-dom';
 import useAuth from 'hooks/useAuth';
 import { fetchCurrentUser } from 'redux/auth/auth-operations';
-import Login from 'Pages/Login';
 
 export const App = () => {
   const { isRefreshing, userToken } = useAuth();
