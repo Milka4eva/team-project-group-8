6803a463978c97dfdfec2ba9aadaa782174661a9
Mila Petruk <mila.petruk@gmail.com>
2023-08-28 20:07:19 +0200

    updated app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GlobalStyle from '../globalStyles';
-import CalendarPage from './../pages/CalendarPage/CalendarPage';
+import CalendarPage from './../Pages/CalendarPage/CalendarPage';
 import { Route, Routes} from 'react-router-dom';
 
 export const App = () => {
