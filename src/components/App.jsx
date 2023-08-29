f81bc79a26d6b7d902ec2002ddf856cdac715521
Mila Petruk <mila.petruk@gmail.com>
2023-08-29 10:28:56 +0200

    changed name Pages into pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import GlobalStyle from '../globalStyles';
-import CalendarPage from './../Pages/CalendarPage/CalendarPage';
+import CalendarPage from './../pages/CalendarPage/CalendarPage';
 import { Route, Routes } from 'react-router-dom';
 import useAuth from 'hooks/useAuth';
 import { fetchCurrentUser } from 'redux/auth/auth-operations';
